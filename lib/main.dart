4f8393f3032f70823e722857513cb0fd0391237e
Ayush Agarwal <aagarwal9782@gmail.com>
2018-06-11 23:04:47 +0530

    made flutter indicator class

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:intro_views_flutter/Models/page_view_model.dart';
 import 'package:intro_views_flutter/UI/pages.dart';
+import 'Animation/page_reveal.dart';
 
 void main() => runApp(new MyApp());
 
@@ -34,10 +34,63 @@ class _MyHomePageState extends State<MyHomePage> {
               pageViewModel: pages[0],
             percentVisible: 1.0,
           ),
-          new Page(
-            pageViewModel: pages[1],
-            percentVisible: 1.0,
-          )
+          new PageReveal(
+            revealPercent: 0.4,
+            child: new Page(
+              pageViewModel: pages[1],
+              percentVisible: 1.0,
+            ),
+          ),
+          new Column(
+            children: <Widget>[
+              new Expanded(child: new Container()),
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: new Container(
+                      width: 20.0,
+                      height: 20.0,
+                      decoration: new BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: const Color(0x88FFFFFF),
+                      ),
+                    ),
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: new Container(
+                      width: 50.0,
+                      height: 50.0,
+                      decoration: new BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: const Color(0x88FFFFFF),
+                      ),
+                      child: new Image.asset(
+                        "assets/wallet.png",
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: new Container(
+                      width: 20.0,
+                      height: 20.0,
+                      decoration: new BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: new Border.all(
+                          color: const Color(0x88FFFFFF),
+                          width: 3.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ],
       ),
 
