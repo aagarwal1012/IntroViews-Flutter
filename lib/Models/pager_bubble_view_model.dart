e5ba785917d0ad2a806c8ea01673a8143182b4fd
Ayush Agarwal <aagarwal9782@gmail.com>
2018-06-11 23:22:29 +0530

    made pager_bubble_view_model class

---
+++ lib/Models/pager_bubble_view_model.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class PagerBubbleViewModel{
+  final String iconAssetPath;
+  final Color color;
+  final bool isHollow;
+  final double activePercent;
+
+  PagerBubbleViewModel(
+      this.iconAssetPath,
+      this.color,
+      this.isHollow,
+      this.activePercent,);
+
+
+}
