37e089787fbf7d988146c233a5188242138cfc5e
Ayush Agarwal <aagarwal9782@gmail.com>
2018-06-12 23:38:14 +0530

    added doc for pager_bubble

--- lib/UI/page_bubble.dart
+++ lib/UI/page_bubble.dart
@@ -3,17 +3,22 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:intro_views_flutter/Models/page_bubble_view_model.dart';
 
+/**
+ * This class contains the UI for page bubble.
+ */
 class PageBubble extends StatelessWidget {
 
+  //view model
   final PageBubbleViewModel viewModel;
 
-
+  //Constructor
   PageBubble({
     this.viewModel,
   });
 
   @override
   Widget build(BuildContext context) {
+
     return new Container(
       width: 65.0,
       height: 65.0,
@@ -21,16 +26,17 @@ class PageBubble extends StatelessWidget {
         child: new Padding(
           padding: const EdgeInsets.all(10.0),
           child: new Container(
-            width: lerpDouble(20.0, 45.0, viewModel.activePercent),
+            width: lerpDouble(20.0, 45.0, viewModel.activePercent), //This method return in between values according to active percent.
             height: lerpDouble(20.0, 45.0, viewModel.activePercent),
             decoration: new BoxDecoration(
               shape: BoxShape.circle,
+              //Alpha is used to create fade effect for background color
               color: viewModel.isHollow ? const Color(0x88FFFFFF).withAlpha((0x88 * viewModel.activePercent).round()) : const Color(0x88FFFFFF),
               border: new Border.all(
                 color: viewModel.isHollow ? const Color(0x88FFFFFF).withAlpha((0x88 * (1 - viewModel.activePercent)).round()) : Colors.transparent,
                 width: 3.0,
-              )
-            ),
+              ),  //Border
+            ),  //BoxDecoration
             child: new Opacity(
               opacity: viewModel.activePercent,
               // ignore: conflicting_dart_import
@@ -38,10 +44,10 @@ class PageBubble extends StatelessWidget {
                 viewModel.iconAssetPath,
                 color: viewModel.color,
               ),
-            ),
-          ),
-        ),
-      ),
-    );
+            ),  //opacity
+          ),  //Container
+        ),  //Padding
+      ),  //Center
+    );  //Container
   }
 }
